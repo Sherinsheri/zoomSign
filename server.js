18b7af0ef0c13ed9655077f167e6472896a20714
FATHIMASHERIN <tve23cs057@cet.ac.in>
2025-07-05 12:42:04 +0530

    first

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  //It redirects the client (browser) to a different URL.
+  res.redirect(`/${uuidV4()}`);
+});
+app.get("/:room", (req, res) => {
+  //Render a dynamic HTML page using a template engine (like EJS, Pug, Handlebars, etc.)
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
