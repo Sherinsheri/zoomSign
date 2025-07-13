99e41a994c466ef4f0667d463666f7b98cbb2c1f
FATHIMASHERIN <tve23cs057@cet.ac.in>
2025-07-13 13:23:32 +0530

    home modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignIn from './Signin';
+//import SignIn from './Signin';
 import Home from './Home';
 import Signin from './Signin';
+import JoinRoom from './components/JoinRoom';
 
 export default function App() {
   return (
     
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/join" element={<JoinRoom />} />
       </Routes>
     </Router>
   );
