35900af22fb174498d35b8960c8eca1160cd2a2f
Mehra Ayisha <mehratacharakkal@gmail.com>
2025-07-13 14:22:03 +0530

    copy roomid

---
+++ client/src/components/clerkTheme.jsx
@@ -0,0 +1,15 @@
+export const darkT = {
+  elements: {
+    formButtonPrimary: {
+      backgroundColor: '#162556 !important',
+      color: '#ffffff !important',
+      border: 'none !important',
+      '&:hover': {
+        backgroundColor: '#1aa5a2 !important',
+      },
+      '&:focus': {
+        backgroundColor: '#189692 !important',
+      },
+    },
+  },
+};
